240cc6ccd0c2c2316173f1100f3b4b30208f4066
Negar <negarsaljooghi@gmail.com>
2024-04-22 10:41:04 +0330

    filter method

--- sanbox.js
+++ sanbox.js
@@ -1,1 +1,15 @@
 const scores = [10, 30, 15, 25, 50, 40, 5];
+
+// filter method example1:
+const filteredScores = scores.filter(score => score > 20);
+console.log(filteredScores);
+
+// filter method example2:
+const users = [
+    { name: 'shaun', premium: true },
+    { name: 'yoshi', premium: false },
+    { name: 'mario', premium: false },
+    { name: 'chun-li', premium: true }
+];
+const premiumUsers = users.filter(user => user.premium);
+console.log(premiumUsers);
