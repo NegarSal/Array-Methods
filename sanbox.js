ad822d582bd40807d6e796346407987486da40b0
Negar <negarsaljooghi@gmail.com>
2024-04-22 11:09:50 +0330

    map  method

--- sanbox.js
+++ sanbox.js
@@ -1,15 +1,22 @@
-const scores = [10, 30, 15, 25, 50, 40, 5];
+// map method example1:
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
 
-// filter method example1:
-const filteredScores = scores.filter(score => score > 20);
-console.log(filteredScores);
+const salesPrices = prices.map(price => price / 2);
+console.log(salesPrices);
 
-// filter method example2:
-const users = [
-    { name: 'shaun', premium: true },
-    { name: 'yoshi', premium: false },
-    { name: 'mario', premium: false },
-    { name: 'chun-li', premium: true }
+// map method example2:
+const products = [
+    { name: 'gold star', price: 20 },
+    { name: 'mushroom', price: 40 },
+    { name: 'green shells', price: 30 },
+    { name: 'banana skin', price: 10 },
+    { name: 'red shells', price: 50 }
 ];
-const premiumUsers = users.filter(user => user.premium);
-console.log(premiumUsers);
+const salesProducts = products.map(product => {
+    if (product.price > 30) {
+        return { name: product.name, price: product.price / 2 }
+    } else {
+        return product;
+    }
+});
+console.log(salesProducts);
