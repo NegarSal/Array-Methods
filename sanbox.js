75927a32bfdae5a6baab160c83442d996fe518e8
Negar <negarsaljooghi@gmail.com>
2024-04-23 09:16:16 +0330

    chain array methode together

--- sanbox.js
+++ sanbox.js
@@ -1,39 +1,21 @@
-// sort method example1 - sorting strings:
-const names = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
-
-names.sort();
-//names.reverse();
-
-console.log(names);
-
-// sort method example2 - sorting numbers:
-const scores = [10, 50, 20, 5, 35, 70, 45];
-
-scores.sort((a, b) => b - a);
-scores.sort((a, b) => a - b);
-
-console.log(scores);
-
-
-// sort method example3 - sorting objects:
-const players = [
-    { name: 'mario', score: 20 },
-    { name: 'luigi', score: 10 },
-    { name: 'chun-li', score: 50 },
-    { name: 'yoshi', score: 30 },
-    { name: 'shaun', score: 70 }
+// chain array methode together:
+const products = [
+    { name: 'gold star', price: 30 },
+    { name: 'green shell', price: 10 },
+    { name: 'red shell', price: 40 },
+    { name: 'banana skin', price: 5 },
+    { name: 'mushroom', price: 50 }
 ];
-players.sort((a, b) => {
-    if (a.score > b.score) {
-        return -1;
-    } else if (b.score > a.score) {
-        return 1;
-    } else {
-        return 0;
-    }
-});
-console.log(players);
 
-// shorter version - sorting objects:
-players.sort((a,b) => b.score - a.score);
-console.log(players);
+// const filtered = products.filter(product => product.price > 20);
+// const promos = filtered.map(product => {
+//     return `the ${product.name} is ${product.price / 2} pounds`
+// });
+// console.log(promos);
+
+
+// shorter version chain array method:
+const promos = products
+    .filter(product => product.price > 20)
+    .map(product => `the ${product.name} is ${product.price / 2} pounds`);
+console.log(promos);
