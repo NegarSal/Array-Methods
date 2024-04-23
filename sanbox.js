27874da1c0225184e9d5b373688c6f9cca6f95a7
Negar <negarsaljooghi@gmail.com>
2024-04-23 09:05:10 +0330

    find  method

--- sanbox.js
+++ sanbox.js
@@ -1,32 +1,5 @@
-// reduce method example1:
-const scores = [10, 20, 60, 40, 70, 90, 30];
-const result = scores.reduce((acc, curr) => {
-    if (curr > 50) {
-        acc++;
-    }
-    return acc;
-}, 0);
-console.log(result);
+// find method:
+const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
-// reduce method example2:
-const scores2 = [
-    { player: 'mario', score: 50 },
-    { player: 'yoshi', score: 30 },
-    { player: 'mario', score: 70 },
-    { player: 'crystal', score: 60 },
-    { player: 'mario', score: 50 },
-    { player: 'yoshi', score: 30 },
-    { player: 'mario', score: 70 },
-    { player: 'crystal', score: 60 },
-    { player: 'mario', score: 30 },
-    { player: 'yoshi', score: 30 },
-    { player: 'mario', score: 90 },
-    { player: 'crystal', score: 60 }
-];
-const marioTotal = scores2.reduce((acc, curr) => {
-    if (curr.player === 'mario') {
-        acc += curr.score;
-    }
-    return acc;
-}, 0);
-console.log(marioTotal);
+const firstHighScore = scores.find(score => score >50);
+console.log(firstHighScore);
