6e7ee8a1e0944bc1d12c0174c162fe746fe018f8
Negar <negarsaljooghi@gmail.com>
2024-04-23 09:12:46 +0330

    sort method - sorting objects

--- sanbox.js
+++ sanbox.js
@@ -13,3 +13,27 @@ scores.sort((a, b) => b - a);
 scores.sort((a, b) => a - b);
 
 console.log(scores);
+
+
+// sort method example3 - sorting objects:
+const players = [
+    { name: 'mario', score: 20 },
+    { name: 'luigi', score: 10 },
+    { name: 'chun-li', score: 50 },
+    { name: 'yoshi', score: 30 },
+    { name: 'shaun', score: 70 }
+];
+players.sort((a, b) => {
+    if (a.score > b.score) {
+        return -1;
+    } else if (b.score > a.score) {
+        return 1;
+    } else {
+        return 0;
+    }
+});
+console.log(players);
+
+// shorter version - sorting objects:
+players.sort((a,b) => b.score - a.score);
+console.log(players);
