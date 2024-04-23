afe6980e2c9cec9d83f304639890272a7b1794d2
Negar <negarsaljooghi@gmail.com>
2024-04-23 09:09:43 +0330

    sort method - sorting numbers

--- sanbox.js
+++ sanbox.js
@@ -5,3 +5,11 @@ names.sort();
 //names.reverse();
 
 console.log(names);
+
+// sort method example2 - sorting numbers:
+const scores = [10, 50, 20, 5, 35, 70, 45];
+
+scores.sort((a, b) => b - a);
+scores.sort((a, b) => a - b);
+
+console.log(scores);
